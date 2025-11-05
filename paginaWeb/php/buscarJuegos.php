253fa7ed00bb5704cb2365c5c0faa4b6eeb3d54f
Miikeel16 <mikel.garcia06@somo.eus>
2025-11-05 08:41:16 +0100

    desplegabla de buscar videojuego

---
+++ paginaWeb/php/buscarJuegos.php
@@ -0,0 +1,22 @@
+<?php
+$conexion = new mysqli("localhost", "root", "", "videojuegos_db");
+
+if ($conexion->connect_error) {
+    die("Error de conexión");
+}
+
+$query = $_GET["query"] ?? "";
+
+$stmt = $conexion->prepare("SELECT id_videojuego, titulo, plataforma FROM videojuego WHERE titulo LIKE ?");
+$like = "%$query%";
+$stmt->bind_param("s", $like);
+$stmt->execute();
+$resultado = $stmt->get_result();
+
+$lista = [];
+while ($fila = $resultado->fetch_assoc()) {
+    $lista[] = $fila;
+}
+
+echo json_encode($lista);
+?>
