7a40104300791051b71c3b73311dc3973a008144
Sendoa6 <sendoa.perez06@somo.eus>
2025-11-12 11:15:32 +0100

    Solo deja entrar si inicia session

--- paginaWeb/php/Sessions/inicio_sesion.php
+++ paginaWeb/php/Sessions/inicio_sesion.php
@@ -11,16 +11,14 @@
     <div class="caja"> <!-- Para reservar el espacio para el formulario -->
         <div class="caja-form"> <!-- Para la estructura de la caja -->
             <h2>Inicio de Sesión</h2>
-            <form action="login_usuario.php" method="post"> <!-- form, es para recopilar datos, action redirige los datos a donde quieras, 
-                                            el "#" hace que no se envien los datos recopilados a ningun lugar externo sino que se quedaran en esta pagina ya que no disponemos de php, method define como se envian los datos, 
-                                            post hace que los datos del formulario se envían de manera segura en el cuerpo de la solicitud, sin ser visibles en la URL.-->
+            <form action="login_usuario.php" method="post"> 
 
                 <label for="username_login">Nombre de Usuario:</label>
-                <input type="text" id="username_login" name="username_login" required>  <!-- Required para que el campo sea obligatorio y text para que te deje escribir en forma de texto -->
+                <input type="text" id="username_login" name="username_login" required>  
 
                 <label for="password_login">Contraseña:</label>
-                <input type="password" id="password_login" name="password_login" required>  <!-- Password para que al escribir no se vea la contraseña -->
-                <button  type="submit">Iniciar Sesión</button><!-- para hacer un boton que haga la funcion de enviar -->
+                <input type="password" id="password_login" name="password_login" required>  ç
+                <button  type="submit">Iniciar Sesión</button>
             </form>
         </div>
     </div>
