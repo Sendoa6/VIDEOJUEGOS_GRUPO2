a6ad4f77f797248102fd2158c80b3c4b84c55651
aneprz <aneperezsenin1@gmail.com>
2025-11-16 20:25:20 +0100

    header cambios

--- paginaWeb/php/index.php
+++ paginaWeb/php/index.php
@@ -15,16 +15,14 @@
 <body>
 
     <!-- HEADER -->
-    <header class="bg-light border-bottom py-3 d-flex justify-content-between align-items-center px-4">
-        <img src="../imagenes/logoGame.png" alt="GAME" class="img-fluid" style="max-width: 10%;">
-        <h1 class="text-center m-0">GESTIÓN DE VIDEOJUEGOS</h1>
-        <a href="../Sessions/cerrar_sesion.php">
-            <img src="../imagenes/iconoCerrarSession.png" 
-                alt="Cerrar sesión" 
-                class="img-fluid" 
-                style="max-width: 6%; cursor: pointer;">
-        </a>
+    <header class="bg-light border-bottom py-3 d-flex align-items-center justify-content-center px-4 gap-3">
+        <img src="../imagenes/logoGame.png" alt="GAME" class="header-img">
 
+        <h1 class="text-center m-0 flex-grow-1">GESTIÓN DE VIDEOJUEGOS</h1>
+
+        <a href="../Sessions/cerrar_sesion.php">
+            <img src="../imagenes/iconoCerrarSession.png" alt="Cerrar sesión" class="header-img-small">
+        </a>
     </header>
 
     <!-- TITULO -->
