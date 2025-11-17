6bc9b7745fa54e851c229e19dfdb85c4fabf07dd
Udaeta <julen.udaeta00@somo.eus>
2025-11-17 08:04:25 +0100

    ip

--- paginaWeb/DataBase/conexiones.php
+++ paginaWeb/DataBase/conexiones.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("54.209.175.92", "php", "", "videojuegos_db");
+$conexion = new mysqli("54.172.200.33", "php", "", "videojuegos_db");
 //$conexion = new mysqli("localhost", "root", "", "videojuegos_db");
 
 // if ($conexion->connect_error) {
